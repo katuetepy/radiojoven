4f64302553868d30d32c84e15a2381cab895b2f8
katuete <juniorbessing1@gmail.com>
2026-04-30 21:04:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // ─────────────────────────────────────────────────
 // CONFIG — Paste ONLY the Google Drive file ID here
 // ─────────────────────────────────────────────────
-const audioID = "SEU_ID_DO_GOOGLE_DRIVE_AQUI";
+const audioID = "1jSHeIZyzNMEmGO6Bg3aIJI0G5SwE5RCH";
 
 // Duration of the audio file in seconds (2 hours)
 const LOOP_DURATION = 7200;
